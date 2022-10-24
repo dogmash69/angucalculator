aff3600a43a4f741cab97cea98224a2e6b7c6256
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-10-24 16:53:29 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatriceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calculatrice/calculatrice.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculatrice',
+  templateUrl: './calculatrice.component.html',
+  styleUrls: ['./calculatrice.component.css']
+})
+export class CalculatriceComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  affichage = '0';
+  valeur= ''
+
+  calcule(value:string) {
+    this.valeur = this.valeur + value
+    this.affichage = this.valeur
+
+  }
+
+  egale(){
+    this.affichage = eval(this.valeur)
+    this.valeur = this.affichage
+  }
+
+  reset() {
+    this.valeur = ''
+    this.affichage = this.valeur
+  }
+}
